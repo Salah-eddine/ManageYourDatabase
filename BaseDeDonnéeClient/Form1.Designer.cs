d1f5d79c6afb443db2063c9c44143aa9ef7082a7
Salaheddine El Bouhlali <el.salaheddine@hotmail.com>
2020-02-05 12:52:37 +0100

    Recuperation des informations sur les colonnes

---
+++ BaseDeDonnéeClient/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BaseDeDonnéeClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tables = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.insertD = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tables
+            // 
+            this.tables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tables.FormattingEnabled = true;
+            this.tables.Items.AddRange(new object[] {
+            "CLIENT",
+            "COMMANDE",
+            "PRODUIT",
+            "DETAIL"});
+            this.tables.Location = new System.Drawing.Point(253, 35);
+            this.tables.Name = "tables";
+            this.tables.Size = new System.Drawing.Size(121, 21);
+            this.tables.TabIndex = 2;
+            this.tables.SelectedIndexChanged += new System.EventHandler(this.tables_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Insert data in the db, select wich table";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // insertD
+            // 
+            this.insertD.Location = new System.Drawing.Point(253, 80);
+            this.insertD.Name = "insertD";
+            this.insertD.Size = new System.Drawing.Size(139, 23);
+            this.insertD.TabIndex = 4;
+            this.insertD.Text = "insert ";
+            this.insertD.UseVisualStyleBackColor = true;
+            this.insertD.Click += new System.EventHandler(this.insertD_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 450);
+            this.Controls.Add(this.insertD);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tables);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox tables;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button insertD;
+    }
+}
+
---
+++ BaseDeDonnéeClient/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseDeDonnéeClient
+{
+    public partial class Form1 : Form
+    {
+        private string chaine_connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\elsal\source\repos\BaseDeDonnéeClient\BaseDeDonnéeClient\BilanCompany.mdf;Integrated Security=True; UID=DESKTOP-RNU0A81/elsal";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            SqlCommand command;
+            SqlDataReader reader; // S'instancie en appelant la méthode ExecuteReader de la classe Sql Commande au lieu de faire appelle à un constructeur
+            SqlConnection cnn;
+            string requete = "SELECT * FROM CLIENT;";
+            cnn = new SqlConnection(this.chaine_connection);
+            cnn.Open();
+            command = new SqlCommand(requete,cnn);
+            reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+               MessageBox.Show(reader.GetName(1)+"\n"+reader.GetString(1));
+            }
+            reader.Close();
+            cnn.Close();
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand command;
+            SqlConnection cnn;
+            String requete;
+            requete = @"INSERT INTO CLIENT VALUES('2343', 'Zak', 'Bruxelles', 'B2', 25444541);";
+            cnn = new SqlConnection(chaine_connection);
+            
+            command = new SqlCommand(requete, cnn);
+            cnn.Open();
+                command.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string s = "Insert data in " + tables.SelectedItem;
+            insertD.Text = s;
+        }
+
+        private void insertD_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            List<string> columns = new List<string>();
+            string tableName,requete;
+            SqlCommand command;
+            SqlConnection cnn;
+            SqlDataReader reader;
+            DataSet ds;
+            tableName = tables.SelectedItem.ToString();
+            requete = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "  +"'"+tableName+"'"+ ";";
+            cnn = new SqlConnection(this.chaine_connection);
+            cnn.Open();
+            command = new SqlCommand(requete, cnn);
+            reader =  command.ExecuteReader();
+            while(reader.Read())
+            {
+                columns.Add(reader.GetString(0));
+               
+            }
+            MessageBox.Show(columns[1]);
+
+
+        }
+    }
+}
